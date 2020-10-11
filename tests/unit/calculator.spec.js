781a390553e2c6b1991fb0cc693585b6c2f2ce11
Callum Wolfe <wolfe.callum@gmail.com>
2020-10-11 15:35:50 +0100

    adds test for division

--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -23,4 +23,11 @@ describe('App.vue', () => {
     wrapper.vm.multiply('5');
     expect(wrapper.vm.runningTotal).to.equal(15);
   })
+
+  it('should divide 21 by 7 and get 3', () => {
+    const wrapper = shallowMount(App);
+    wrapper.vm.previousTotal = 21;
+    wrapper.vm.divide('7');
+    expect(wrapper.vm.runningTotal).to.equal(3);
+  })
 })
