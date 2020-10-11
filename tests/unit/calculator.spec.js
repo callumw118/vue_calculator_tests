9463974d58a172bd3cad569a59d31385f64a3f1f
Callum Wolfe <wolfe.callum@gmail.com>
2020-10-11 15:34:21 +0100

    adds test for mutiplication

--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -4,10 +4,10 @@ import App from '@/App.vue'
 
 describe('App.vue', () => {
   it('enterNum changes running total', () => {
-    const wrapper = shallowMount(App)
-    wrapper.vm.previousTotal = 4
+    const wrapper = shallowMount(App);
+    wrapper.vm.previousTotal = 4;
     wrapper.vm.add('5');
-    expect(wrapper.vm.runningTotal).to.equal(9)
+    expect(wrapper.vm.runningTotal).to.equal(9);
   })
 
   it('should subtract 4 from 7 and get 3', () => {
@@ -15,5 +15,12 @@ describe('App.vue', () => {
     wrapper.vm.previousTotal = 7;
     wrapper.vm.subtract('4');
     expect(wrapper.vm.runningTotal).to.equal(3);
+  })
+
+  it('should multiply 3 by 5 and get 15', () =>{
+    const wrapper = shallowMount(App);
+    wrapper.vm.previousTotal = 3;
+    wrapper.vm.multiply('5');
+    expect(wrapper.vm.runningTotal).to.equal(15);
   })
 })
