a4c6f0f0149daa2f5a0b16789bfd5b934ffa4156
Callum Wolfe <wolfe.callum@gmail.com>
2020-10-11 16:33:16 +0100

    adds tests for other arithmetical operations

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -17,4 +17,28 @@ describe('calculator', () => {
     cy.get('#operator_equals').click();
     cy.get('.display').should('contain', '5');
   })
+
+  it('should subtract two numbers and display the total', () => {
+    cy.get('#number3').click();
+    cy.get('#operator_subtract').click();
+    cy.get('#number2').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '1');
+  })
+
+  it('should multiply two numbers and display the total', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_multiply').click();
+    cy.get('#number3').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '6');
+  })
+
+  it('should divide two numbers and display the total', () => {
+    cy.get('#number4').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number2').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '2');
+  })
 })
