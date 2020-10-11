cd290299f3e895271bd122764e67d386de226ce9
Callum Wolfe <wolfe.callum@gmail.com>
2020-10-11 15:32:19 +0100

    test for subtraction

--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -9,4 +9,11 @@ describe('App.vue', () => {
     wrapper.vm.add('5');
     expect(wrapper.vm.runningTotal).to.equal(9)
   })
+
+  it('should subtract 4 from 7 and get 3', () => {
+    const wrapper = shallowMount(App);
+    wrapper.vm.previousTotal = 7;
+    wrapper.vm.subtract('4');
+    expect(wrapper.vm.runningTotal).to.equal(3);
+  })
 })
